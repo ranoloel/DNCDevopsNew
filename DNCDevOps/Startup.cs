018351fcdcc1f0bdcf5f092772cbd4738574c278
ranoloel <elmoranolo@gmail.com>
2020-11-03 21:40:39 +0800

    Adding stackify middleware

--- DNCDevOps/Startup.cs
+++ DNCDevOps/Startup.cs
@@ -29,6 +29,9 @@ namespace DNCDevOps
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMiddleware<StackifyMiddleware.RequestTracerMiddleware>();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
